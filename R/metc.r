3155777911cdb16141a5cb0c009ce448d5787c8f
openfields <will.openfields@gmail.com>
2016-07-07 13:12:04 -0400

    added code to calculate patch importance values

--- R/metc.r
+++ R/metc.r
@@ -6,10 +6,10 @@ metc <- function(dmat, alpha){
   dmat*(-1/alpha) -> dmat2
   exp(dmat2) -> M.mat
   eigen(M.mat,symmetric = TRUE, only.values = FALSE) -> M.eig
-  return(M.eig$values[1])
+
+  M.eig$vectors[,1]->lead.ev
+  lead.ev^2*M.eig$values[1] -> patch
+  return(list(mc=M.eig$values[1],patch=patch))
     
-  
 }
 
-# M.eig$vectors[,1]->lead.ev
-# lead.ev^2*M.eig$values[1] -> patch.im

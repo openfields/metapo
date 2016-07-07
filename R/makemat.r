7074a5d091057c320afbb0e4f737c4b017173f5b
openfields <will.openfields@gmail.com>
2016-07-07 10:26:10 -0400

    fixed bug so data are referencing parameter passed to function: dtable

--- R/makemat.r
+++ R/makemat.r
@@ -5,26 +5,26 @@ makemat <- function(dtable, n_eles){
   matrix(-1, nrow=n_eles, ncol=n_eles) -> dmat
   
   # concatenate data and copy first row
-  c(0,pdata[1:(n_eles-1),4]) -> dmat[1,]
+  c(0,dtable[1:(n_eles-1),4]) -> dmat[1,]
   
   # get data for rows 2 - (nrow-2)
   for(i in 2:(n_eles-2)){
-    which(pdata$from_cat==i) -> tmp # index of 607 elements
-    pdata[tmp,4] -> dists # 607 pairwise distances
+    which(dtable$from_cat==i) -> tmp # index of 607 elements
+    dtable[tmp,4] -> dists # 607 pairwise distances
     dat <- c(dists[1:(i-1)],0,dists[i:607]) 
     dmat[i,]<-dat
   }
   #penultimate row
   i <- i + 1
-  which(pdata$from_cat==i) -> tmp
-  pdata[tmp,4] -> dists
+  which(dtable$from_cat==i) -> tmp
+  dtable[tmp,4] -> dists
   dat <- c(dists[1:606],0,dists[607])
   dmat[i,] <- dat
   i <- i + 1
   # 
   # # final row
-  which(pdata$from_cat==i) -> tmp
-  pdata[tmp,4] -> dists
+  which(dtable$from_cat==i) -> tmp
+  dtable[tmp,4] -> dists
   dat <- c(dists,0)
   dmat[i,] <- dat
 

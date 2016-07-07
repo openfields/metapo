39f2355bc67e46f21006a993b8b089a96aea0422
openfields <will.openfields@gmail.com>
2016-07-07 12:36:10 -0400

    modify function to use first element for dominant eigenvalue

--- R/metc.r
+++ R/metc.r
@@ -5,9 +5,11 @@ metc <- function(dmat, alpha){
   
   dmat*(-1/alpha) -> dmat2
   exp(dmat2) -> M.mat
-  eigen(M.mat,symmetric = TRUE, only.values = TRUE) -> M.eig
-  return(max(M.eig$values))
-  
+  eigen(M.mat,symmetric = TRUE, only.values = FALSE) -> M.eig
+  return(M.eig$values[1])
+    
   
 }
 
+# M.eig$vectors[,1]->lead.ev
+# lead.ev^2*M.eig$values[1] -> patch.im

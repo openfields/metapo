515a7124e153c3f51b30614606b642a19899db09
Will Fields <will.openfields@gmail.com>
2018-05-10 10:02:35 -0500

    metapopulation metrics calculated with rARPACK library

--- R/mcarp.r
+++ R/mcarp.r
@@ -1,15 +1,15 @@
-metc <- function(dmat, alpha){
+metarpack <- function(dmat, alpha){
   # calculate metapopulation capacity
   # input: distance matrix, alpha
   # returns metapopulation capacity
   
   # require rARPACK
-  library(rARPACK)
+  require(rARPACK)
   
   dmat*(-1/alpha) -> dmat2
   exp(dmat2) -> M.mat
   
-  eigs_sym(M.mat, k=5, lower=TRUE)
+  eigs_sym(M.mat, k=5, lower=TRUE) -> M.eig
  # eigen(M.mat,symmetric = TRUE, only.values = FALSE) -> M.eig
   
   M.eig$vectors[,1]->lead.ev
@@ -18,5 +18,12 @@ metc <- function(dmat, alpha){
   
 }
 
-system.time(eigs_sym(a42e.d, k=5, lower=TRUE) -> foo)
-system.time(eigen(a42e.d ,symmetric = TRUE, only.values = FALSE) -> M.eig)
+
+# system.time(metarpack(a42e.d, 5000) -> arp.5k) # 2651 sec for old function, 17.71 for this
+
+
+
+
+
+# system.time(eigs_sym(a42e.d, k=5, lower=TRUE) -> foo)
+# system.time(eigen(a42e.d ,symmetric = TRUE, only.values = FALSE) -> M.eig)

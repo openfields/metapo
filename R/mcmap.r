1197dda847b2aa5e6f4b53243984369055538251
Will Fields <will.openfields@gmail.com>
2018-08-13 16:19:48 -0500

    tried removing vector data and doing garbage collection to free memory

--- R/mcmap.r
+++ R/mcmap.r
@@ -7,13 +7,20 @@ require(rARPACK)
 readVECT(eval(vname[1])) -> vecdata 
 # get coords
 coordinates(vecdata) -> vecdata_pt
+
+# remove vector data and try garbage collection
+vecdata@data -> nd
+rm(vecdata)
+nd -> vecdata
+gc()
+
 # calc dist, convert to matrix
 dist(vecdata_pt) -> vecdata_dist
 as.matrix(vecdata_dist) -> vecdata_d
 # create area matrix
-length(vecdata@data$area) -> v
-tmp1 <- matrix(rep(vecdata@data$area,v), ncol=v, nrow=v, byrow=TRUE)
-tmp2 <- matrix(rep(vecdata@data$area,v), ncol=v, nrow=v, byrow=FALSE)
+length(vecdata$area) -> v
+tmp1 <- matrix(rep(vecdata$area,v), ncol=v, nrow=v, byrow=TRUE)
+tmp2 <- matrix(rep(vecdata$area,v), ncol=v, nrow=v, byrow=FALSE)
 tmp3 <- tmp1*tmp2
 # discounting for distance & area
 vecdata_d*(-1/dd) -> dm
@@ -23,7 +30,7 @@ m*tmp3 -> M.mat
 eigs_sym(M.mat, k=5, lower=TRUE) -> M.eig
 M.eig$vectors[,1] -> lead.ev
 lead.ev^2 * M.eig$values[1] -> patch
-list(mc=M.eig$values[1], patch=patch, fid=vecdata@data[,1]) -> out
+list(mc=M.eig$values[1], patch=patch, fid=vecdata[,1]) -> out
 # return output
 return(out)
 }

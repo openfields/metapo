a313471010891fd7333d4adfb6a200d46a87a9ca
Will Fields <will.openfields@gmail.com>
2018-10-24 14:02:00 -0500

    script for aquatic networks

--- R/aq_netstats.r
+++ R/aq_netstats.r
@@ -202,8 +202,8 @@ read.dbf('C:/Users/u4cnnwrf/Documents/gis/fril_padtosh.dbf') -> flw_tosh
 which(flw_tosh$b_cat>=0) -> i.ftosh
 flw_tosh[i.ftosh,] -> ftosh
 
-netown(ftosh, frll.m) -> ftosh.m
-netgap(ftosh, frll.g) -> ftosh.g
+netown(ftosh, ftosh.m) -> ftosh.m
+netgap(ftosh, ftosh.g) -> ftosh.g
 
 rep('ftosh', dim(ftosh.m)[1]) -> ftosh.mname
 rep('ftosh', dim(ftosh.g)[1]) -> ftosh.gname
@@ -615,5 +615,316 @@ humb.own
 humb.gap
 
 # jblmcr_howel
+read.dbf('F:/report_data/arcgis_data/jblmcr_padhowel.dbf') -> jb_phow
+str(jb_phow)
+which(jb_phow$FID_jblm_1>=0) -> i.how ##
+jb_phow[i.how,] -> jhow
+
+netown(jhow, jhow.m) -> jhow.m
+netgap(jhow, jhow.g) -> jhow.g
+
+rep('jhow', dim(jhow.m)[1]) -> jhow.mname
+rep('jhow', dim(jhow.g)[1]) -> jhow.gname
+
+cbind(jhow.mname, jhow.m) -> jhow.own
+names(jhow.own) <- c('net', 'own', 'area')
+
+cbind(jhow.gname, jhow.g) -> jhow.gap
+names(jhow.gap) <- c('net', 'gap', 'area')
+
+jhow.own
+jhow.gap
+
+# jblmcr_orsf
+read.dbf('F:/report_data/arcgis_data/jblmcr_padorsf.dbf') -> jb_porsf
+str(jb_porsf)
+which(jb_porsf$b_cat>=0) -> i.jfrog ##
+jb_porsf[i.jfrog,] -> jfrog
+
+netown(jfrog, jfrog.m) -> jfrog.m
+netgap(jfrog, jfrog.g) -> jfrog.g
+
+rep('jfrog', dim(jfrog.m)[1]) -> jfrog.mname
+rep('jfrog', dim(jfrog.g)[1]) -> jfrog.gname
+
+cbind(jfrog.mname, jfrog.m) -> jfrog.own
+names(jfrog.own) <- c('net', 'own', 'area')
+
+cbind(jfrog.gname, jfrog.g) -> jfrog.gap
+names(jfrog.gap) <- c('net', 'gap', 'area')
+
+jfrog.own
+jfrog.gap
+
+# jblmcr_padsalmon
+read.dbf('F:/report_data/arcgis_data/jblmcr_padsalmon.dbf') -> jb_psalm
+str(jb_psalm)
+which(jb_psalm$FID_jblm_1>=0) -> i.jsal ##
+jb_psalm[i.jsal,] -> jsalm
+
+netown(jsalm, jsalm.m) -> jsalm.m
+netgap(jsalm, jsalm.g) -> jsalm.g
+
+rep('jsalm', dim(jsalm.m)[1]) -> jsalm.mname
+rep('jsalm', dim(jsalm.g)[1]) -> jsalm.gname
+
+cbind(jsalm.mname, jsalm.m) -> jsalm.own
+names(jsalm.own) <- c('net', 'own', 'area')
+
+cbind(jsalm.gname, jsalm.g) -> jsalm.gap
+names(jsalm.gap) <- c('net', 'gap', 'area')
+
+jsalm.own
+jsalm.gap
+
+# jblmcr_padtrout
+read.dbf('F:/report_data/arcgis_data/jblmcr_padtrout.dbf') -> jb_ptrout
+str(jb_ptrout)
+which(jb_ptrout$FID_jblm_1>=0) -> i.jtr ##
+jb_ptrout[i.jtr,] -> jtrou
+
+netown(jtrou, jtrou.m) -> jtrou.m
+netgap(jtrou, jtrou.g) -> jtrou.g
+
+rep('jtrou', dim(jtrou.m)[1]) -> jtrou.mname
+rep('jtrou', dim(jtrou.g)[1]) -> jtrou.gname
+
+cbind(jtrou.mname, jtrou.m) -> jtrou.own
+names(jtrou.own) <- c('net', 'own', 'area')
+
+cbind(jtrou.gname, jtrou.g) -> jtrou.gap
+names(jtrou.gap) <- c('net', 'gap', 'area')
+
+jtrou.own
+jtrou.gap
+
+# mccoy_padmass
+read.dbf('F:/report_data/arcgis_data/mccoy_padmass.dbf') -> mc_pmas
+str(mc_pmas)
+which(mc_pmas$FID_mccoy1>=0) -> i.mpm ##
+mc_pmas[i.mpm,] -> mcmas
+
+netown(mcmas, mcmas.m) -> mcmas.m
+netgap(mcmas, mcmas.g) -> mcmas.g
+
+rep('mcmas', dim(mcmas.m)[1]) -> mcmas.mname
+rep('mcmas', dim(mcmas.g)[1]) -> mcmas.gname
+
+cbind(mcmas.mname, mcmas.m) -> mcmas.own
+names(mcmas.own) <- c('net', 'own', 'area')
+
+cbind(mcmas.gname, mcmas.g) -> mcmas.gap
+names(mcmas.gap) <- c('net', 'gap', 'area')
+
+mcmas.own
+mcmas.gap
+
+# mta_padmass
+read.dbf('F:/report_data/arcgis_data/mta_padmass.dbf') -> mt_pmas
+str(mt_pmas)
+which(mt_pmas$FID_mta_nw>=0) -> i.mtm ##
+mt_pmas[i.mtm,] -> mtma
+
+netown(mtma, mtma.m) -> mtma.m
+netgap(mtma, mtma.g) -> mtma.g
+
+rep('mtma', dim(mtma.m)[1]) -> mtma.mname
+rep('mtma', dim(mtma.g)[1]) -> mtma.gname
+
+cbind(mtma.mname, mtma.m) -> mtma.own
+names(mtma.own) <- c('net', 'own', 'area')
+
+cbind(mtma.gname, mtma.g) -> mtma.gap
+names(mtma.gap) <- c('net', 'gap', 'area')
+
+mtma.own
+mtma.gap
+
+# prfta_padcats
+read.dbf('F:/report_data/arcgis_data/prfta_padcats.dbf') -> pr_pcat
+str(pr_pcat)
+which(pr_pcat$FID_prfta1>=0) -> i.ppc ##
+pr_pcat[i.ppc,] -> pcat
+
+netown(pcat, pcat.m) -> pcat.m
+netgap(pcat, pcat.g) -> pcat.g
+
+rep('pcat', dim(pcat.m)[1]) -> pcat.mname
+rep('pcat', dim(pcat.g)[1]) -> pcat.gname
+
+cbind(pcat.mname, pcat.m) -> pcat.own
+names(pcat.own) <- c('net', 'own', 'area')
+
+cbind(pcat.gname, pcat.g) -> pcat.gap
+names(pcat.gap) <- c('net', 'gap', 'area')
+
+pcat.own
+pcat.gap
+
+# prfta_padcrlf
+read.dbf('F:/report_data/arcgis_data/prfta_padcrlf.dbf') -> pr_pf
+str(pr_pf)
+which(pr_pf$FID_prfta1>=0) -> i.ppf ##
+pr_pf[i.ppf,] -> pfrog
+
+netown(pfrog, pfrog.m) -> pfrog.m
+netgap(pfrog, pfrog.g) -> pfrog.g
+
+rep('pfrog', dim(pfrog.m)[1]) -> pfrog.mname
+rep('pfrog', dim(pfrog.g)[1]) -> pfrog.gname
+
+cbind(pfrog.mname, pfrog.m) -> pfrog.own
+names(pfrog.own) <- c('net', 'own', 'area')
+
+cbind(pfrog.gname, pfrog.g) -> pfrog.gap
+names(pfrog.gap) <- c('net', 'gap', 'area')
+
+pfrog.own
+pfrog.gap
+
+# shel_paddart
+read.dbf('F:/report_data/arcgis_data/shel_paddart.dbf') -> sh_pdart
+str(sh_pdart)
+which(sh_pdart$FID_shel_d>=0) -> i.sd ##
+sh_pdart[i.sd,] -> sdart
+
+netown(sdart, sdart.m) -> sdart.m
+netgap(sdart, sdart.g) -> sdart.g
+
+rep('sdart', dim(sdart.m)[1]) -> sdart.mname
+rep('sdart', dim(sdart.g)[1]) -> sdart.gname
+
+cbind(sdart.mname, sdart.m) -> sdart.own
+names(sdart.own) <- c('net', 'own', 'area')
+
+cbind(sdart.gname, sdart.g) -> sdart.gap
+names(sdart.gap) <- c('net', 'gap', 'area')
+
+sdart.own
+sdart.gap
+
+# shel_padquil
+read.dbf('F:/report_data/arcgis_data/shel_padquil.dbf') -> sh_pquil
+str(sh_pquil)
+which(sh_pquil$FID_shel_q>=0) -> i.squ ##
+sh_pquil[i.squ,] -> squi
+
+netown(squi, squi.m) -> squi.m
+netgap(squi, squi.g) -> squi.g
+
+rep('squi', dim(squi.m)[1]) -> squi.mname
+rep('squi', dim(squi.g)[1]) -> squi.gname
+
+cbind(squi.mname, squi.m) -> squi.own
+names(squi.own) <- c('net', 'own', 'area')
+
+cbind(squi.gname, squi.g) -> squi.gap
+names(squi.gap) <- c('net', 'gap', 'area')
+
+squi.own
+squi.gap
+
+#######################################################################
+# FIXING NA'S & WRITING OUTPUT FILE
+#######################################################################
+# use find& replace for efficiency
+# write out data
+write.csv(brho.own, './data/net_own/brho_own.csv', row.names=FALSE)
+write.csv(brpm.own, './data/net_own/brpm_own.csv', row.names=FALSE)
+write.csv(bswf.own, './data/net_own/bswf_own.csv', row.names=FALSE)
+write.csv(cmass.own, './data/net_own/cmass_own.csv', row.names=FALSE)
+write.csv(crabe.own, './data/net_own/crabe_own.csv', row.names=FALSE)
+write.csv(csnuf.own, './data/net_own/csnuf_own.csv', row.names=FALSE)
+write.csv(ffs.own, './data/net_own/ffs_own.csv', row.names=FALSE)
+write.csv(flance.own, './data/net_own/flance_own.csv', row.names=FALSE)
+write.csv(fpink.own, './data/net_own/fpink_own.csv', row.names=FALSE)
+write.csv(fpipl.own, './data/net_own/fpipl_own.csv', row.names=FALSE)
+write.csv(frll.own, './data/net_own/frll_own.csv', row.names=FALSE)
+write.csv(frlog.own, './data/net_own/frlog_own.csv', row.names=FALSE)
+write.csv(fsfs.own, './data/net_own/fsfs_own.csv', row.names=FALSE)
+write.csv(fspec.own, './data/net_own/fspec_own.csv', row.names=FALSE)
+write.csv(fst.own, './data/net_own/fst_own.csv', row.names=FALSE)
+write.csv(ftosh.own, './data/net_own/ftosh_own.csv', row.names=FALSE)
+write.csv(ggrod.own, './data/net_own/ggrod_own.csv', row.names=FALSE)
+write.csv(gjrll.own, './data/net_own/gjrll_own.csv', row.names=FALSE)
+write.csv(gmas.own, './data/net_own/gmas_own.csv', row.names=FALSE)
+write.csv(hcats.own, './data/net_own/hcats_own.csv', row.names=FALSE)
+write.csv(hfrog.own, './data/net_own/hfrog_own.csv', row.names=FALSE)
+write.csv(hshrimp.own, './data/net_own/hshrimp_own.csv', row.names=FALSE)
+write.csv(hspike.own, './data/net_own/hspike_own.csv', row.names=FALSE)
+write.csv(hsteel.own, './data/net_own/hsteel_own.csv', row.names=FALSE)
+write.csv(hswfl.own, './data/net_own/hswfl_own.csv', row.names=FALSE)
+write.csv(htig.own, './data/net_own/htig_own.csv', row.names=FALSE)
+write.csv(htoad.own, './data/net_own/htoad_own.csv', row.names=FALSE)
+write.csv(huchub.own, './data/net_own/huchub_own.csv', row.names=FALSE)
+write.csv(humb.own, './data/net_own/humb_own.csv', row.names=FALSE)
+write.csv(hup.own, './data/net_own/hup_own.csv', row.names=FALSE)
+write.csv(hupem.own, './data/net_own/hupem_own.csv', row.names=FALSE)
+write.csv(hvir.own, './data/net_own/hvir_own.csv', row.names=FALSE)
+write.csv(jfrog.own, './data/net_own/jfrog_own.csv', row.names=FALSE)
+write.csv(jhow.own, './data/net_own/jhow_own.csv', row.names=FALSE)
+write.csv(jsalm.own, './data/net_own/jsalm_own.csv', row.names=FALSE)
+write.csv(jtrou.own, './data/net_own/jtrou_own.csv', row.names=FALSE)
+write.csv(mcmas.own, './data/net_own/mcmas_own.csv', row.names=FALSE)
+write.csv(mtma.own, './data/net_own/mtma_own.csv', row.names=FALSE)
+write.csv(pcat.own, './data/net_own/pcat_own.csv', row.names=FALSE)
+write.csv(pfrog.own, './data/net_own/pfrog_own.csv', row.names=FALSE)
+write.csv(squi.own, './data/net_own/squi_own.csv', row.names=FALSE)
+write.csv(sdart.own, './data/net_own/sdart_own.csv', row.names=FALSE)
 
 
+
+# read in data - it got the NA's replaced with 0's
+read.csv('./data/net_own/brho_own.csv', header=TRUE) -> brho_own
+read.csv('./data/net_own/brpm_own.csv', header=TRUE) -> brpm_own
+read.csv('./data/net_own/bswf_own.csv', header=TRUE) ->bswf_own
+read.csv('./data/net_own/cmass_own.csv', header=TRUE) ->cmass_own
+read.csv('./data/net_own/crabe_own.csv', header=TRUE) ->crabe_own
+read.csv('./data/net_own/csnuf_own.csv', header=TRUE)->csnuf_own
+read.csv('./data/net_own/ffs_own.csv', header=TRUE)->ffs_own
+read.csv('./data/net_own/flance_own.csv', header=TRUE)->flance_own
+read.csv('./data/net_own/fpink_own.csv', header=TRUE)->fpink_own
+read.csv('./data/net_own/fpipl_own.csv', header=TRUE)->fpipl_own
+read.csv('./data/net_own/frll_own.csv', header=TRUE)->frll_own
+read.csv('./data/net_own/frlog_own.csv', header=TRUE)->frlog_own
+read.csv('./data/net_own/fsfs_own.csv', header=TRUE)->fsfs_own
+read.csv('./data/net_own/fspec_own.csv', header=TRUE)->fspec_own
+read.csv('./data/net_own/fst_own.csv', header=TRUE)->fst_own
+read.csv('./data/net_own/ftosh_own.csv', header=TRUE)->ftosh_own
+read.csv('./data/net_own/ggrod_own.csv', header=TRUE)->ggrod_own
+read.csv('./data/net_own/gjrll_own.csv', header=TRUE)->gjrll_own
+read.csv('./data/net_own/gmas_own.csv', header=TRUE)->gmas_own
+read.csv('./data/net_own/hcats_own.csv', header=TRUE)->hcats_own
+read.csv('./data/net_own/hfrog_own.csv', header=TRUE)->hfrog_own
+read.csv('./data/net_own/hshrimp_own.csv', header=TRUE)->hshrimp_own
+read.csv('./data/net_own/hspike_own.csv', header=TRUE)->hspike_own
+read.csv('./data/net_own/hsteel_own.csv', header=TRUE)->hsteel_own
+read.csv('./data/net_own/hswfl_own.csv', header=TRUE)->hswfl_own
+read.csv('./data/net_own/htig_own.csv', header=TRUE)->htig_own
+read.csv('./data/net_own/htoad_own.csv', header=TRUE)->htoad_own
+read.csv('./data/net_own/huchub_own.csv', header=TRUE)->huchub_own
+read.csv('./data/net_own/humb_own.csv', header=TRUE)->humb_own
+read.csv('./data/net_own/hup_own.csv', header=TRUE)->hup_own
+read.csv('./data/net_own/hupem_own.csv', header=TRUE)->hupem_own
+read.csv('./data/net_own/hvir_own.csv', header=TRUE)->hvir_own
+read.csv('./data/net_own/jfrog_own.csv', header=TRUE)->jfrog_own
+read.csv('./data/net_own/jhow_own.csv', header=TRUE)->jhow_own
+read.csv('./data/net_own/jsalm_own.csv', header=TRUE)->jsalm_own
+read.csv('./data/net_own/jtrou_own.csv', header=TRUE)->jtrou_own
+read.csv('./data/net_own/mcmas_own.csv', header=TRUE)->mcmas_own
+read.csv('./data/net_own/mtma_own.csv', header=TRUE)->mtma_own
+read.csv('./data/net_own/pcat_own.csv', header=TRUE)->pcat_own
+read.csv('./data/net_own/pfrog_own.csv', header=TRUE)->pfrog_own
+read.csv('./data/net_own/squi_own.csv', header=TRUE)->squi_own
+read.csv('./data/net_own/sdart_own.csv', header=TRUE)->sdart_own
+
+# bundle data to cast
+rbind(brho_own, brpm_own, bswf_own, cmass_own, crabe_own, csnuf_own, ffs_own, flance_own, fpink_own, fpipl_own, frll_own, frlog_own, fsfs_own, fspec_own, fst_own, 
+      ftosh_own, ggrod_own, gjrll_own, gmas_own, hcats_own, hfrog_own, hshrimp_own, hspike_own, hsteel_own, hswfl_own, htig_own, htoad_own, huchub_own, humb_own, 
+      hup_own, hupem_own, hvir_own, jfrog_own, jhow_own, jsalm_own, jtrou_own, mcmas_own, mtma_own, pcat_own, pfrog_own, sdart_own, squi_own) -> aq.net.own  
+library(dplyr)
+library(reshape)
+cast(aq.net.own, net~own) -> cast.aq
+cast.aq
+# export to csv; replace NA's with 0's in Excel
+write.csv(cast.aq, './data/aq_net_own.csv', row.names = FALSE)
